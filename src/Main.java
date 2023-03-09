8f3743b803610164a2a0d5b3a3466e5c8c208375
TomWulf <Tom.Wulf@uc.edu>
2023-03-09 14:05:50 -0500

    Ready for Demo Created Method Version of the Selection Sort

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import org.jetbrains.annotations.NotNull;
+
 import java.util.Scanner;
 import java.util.Random;
 
@@ -48,8 +50,30 @@ public class Main {
 
         for(int p = 0; p < data.length; p++)
             System.out.printf("%3d  ", data[p]);
+    }
 
+    private static void SelectionSort(int @NotNull [] arr)
+    {
+        for(int x=0; x < arr.length; x++)
+        {
+            // Find the minimum
+            minVal = arr[x]; // Min is 1st value until we compare and find a new one
+            minDex = x; // index of the lo0cation of the min value
 
+            // scan for a new min starting at this location NOT at the beginning
+            // because the portion of the aray before x has been sorted already
 
+            for(int s = x; s < arr.length; s++) {
+                if (arr[s] < minVal) {  // if its a new min
+                    minVal = arr[s];    // rememner it
+                    minDex = s;          // and it's index
+                }
+            }
+            // now swap the element into place
+            swap = arr[x];  // this is where the new min will go in the sorted order save whats there already with swap
+            arr[x] = arr[minDex]; // over write it putting the min vlaue found in its proper place in the sort
+            arr[minDex] = swap;    // put the value that you overwrite back in the location of the found min to complete the swap
+
+        }
     }
 }
