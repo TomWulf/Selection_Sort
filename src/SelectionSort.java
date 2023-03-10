aad74cd06fed888f206df50a56f115a8b08c5c03
TomWulf <Tom.Wulf@uc.edu>
2023-03-10 09:24:32 -0500

    Ready for Demo Created Method Version of the Selection Sort Now Debugged

--- src/SelectionSort.java
+++ src/SelectionSort.java
@@ -3,13 +3,13 @@ import org.jetbrains.annotations.NotNull;
 import java.util.Scanner;
 import java.util.Random;
 
-public class Main {
+public class SelectionSort {
     public static void main(String[] args) {
 
         Scanner in = new Scanner(System.in);
         Random rnd = new Random();
 
-        final int DATA_LENGTH = 100;
+        final int DATA_LENGTH = 1000;
 
         int[] data = new int[DATA_LENGTH];
 
@@ -23,22 +23,30 @@ public class Main {
         }
 
 
-        // selection sort
+        /*
+           Selection sort
+           Traverse the array each time picking the min and swapping it back into the array at the sorted position at the front
+           As the sort progresses the first part of the array is sorted with the loop continuing to pick the next minimum
+           until the entire array is sorted.
+         */
+
+
         for(int x=0; x < data.length; x++)
         {
-            // Find the minimum
-            minVal = data[x]; // Min is 1st value until we compare and find a new one
-            minDex = x; // index of the lo0cation of the min value
+            // Find the minimum assume that it is initially the first element of the unsorted region
+            minVal = data[x]; // Min is 1st value of unsorted region until we compare and find a new one
+            minDex = x; // index of the location of the min value
 
-            // scan for a new min starting at this location NOT at the beginning
-            // because the portion of the aray before x has been sorted already
+            // scan for a new min starting at this location NOT at the beginning of the entire array
+            // because the portion of the array before x has been sorted already
 
             for(int s = x; s < data.length; s++) {
+                   // Notice how this loop starts at x so it only traverses the remaining unsorted region of the array
                 if (data[s] < minVal) {  // if its a new min
-                    minVal = data[s];    // rememner it
+                    minVal = data[s];    // remember it
                     minDex = s;          // and it's index
                 }
-            }
+            }  // When this loop completes we have done a traversal to pick the min from the unsorted region
             // now swap the element into place
             swap = data[x];  // this is where the new min will go in the sorted order save whats there already with swap
             data[x] = data[minDex]; // over write it putting the min vlaue found in its proper place in the sort
@@ -54,6 +62,10 @@ public class Main {
 
     private static void SelectionSort(int @NotNull [] arr)
     {
+        int minVal;
+        int minDex;
+        int swap;
+
         for(int x=0; x < arr.length; x++)
         {
             // Find the minimum
